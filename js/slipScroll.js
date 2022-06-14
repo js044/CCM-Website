add339d81c7e4d0f06a6042aae1801bfbe122d23
js044 <js044@mailbox.org>
2022-06-13 18:53:26 -0600

    Initial commit

---
+++ js/slipScroll.js
@@ -0,0 +1,18 @@
+var setLogo = function() {
+
+  $('.moveable').each(function() {
+    $(this).css('top',
+      $('.default').offset().top -
+      $(this).closest('.container').offset().top
+    );
+  });
+
+};
+
+$(document).scroll(function() {
+  setLogo();
+});
+
+setLogo();
+
+
